5644dbb67aafdb6a19607ab33cf58c63f08cee55
willy95 <wilvardo@gmail.com>
2017-12-14 17:22:43 -0600

    Log

--- public/scripts/encuesta.js
+++ public/scripts/encuesta.js
@@ -1,6 +1,7 @@
 'use strict'
 
 $('#singleEnviar').click(function(e){
+    e.preventDefault();
     var quest = $("label[id*='question']").length;
     var yes = $('.yes:checked').length;
     var promedio = (yes * 100) / quest
